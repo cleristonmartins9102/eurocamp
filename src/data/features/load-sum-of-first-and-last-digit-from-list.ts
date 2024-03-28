e23782bc9e601603df7c52586a1d3d98fc211632
Martins Cleriston <cleriston.mari@gmail.com>
2024-03-28 17:52:14 +0000

    feat: refactor LoadSumOfFirstAndLastDigitFromList changed variables name

--- src/data/features/load-sum-of-first-and-last-digit-from-list.ts
+++ src/data/features/load-sum-of-first-and-last-digit-from-list.ts
@@ -1,8 +1,8 @@
 export class LoadSumOfFirstAndLastDigitFromList {
-  load (list: string[]): number {
+  load (lines: string[]): number {
     let calc = 0
-    for (const row of list) {
-      const onlyNumbers = row.split('').filter(value => /\d+/g.test(value)) ?? []
+    for (const line of lines) {
+      const onlyNumbers = line.split('').filter(value => /\d+/g.test(value)) ?? []
       if (onlyNumbers?.length > 0 && typeof onlyNumbers[0] !== 'undefined' && onlyNumbers[0] !== null) {
         calc = onlyNumbers.length === 1
           ? calc + parseInt(`${onlyNumbers[0] + onlyNumbers[0]}`)
